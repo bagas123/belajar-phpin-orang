577fcd8bfebcf872144a2026e9b40b340ead917f
bagas <bagasdewantoro1999@gmail.com>
2017-02-24 20:27:47 +0700

    Adding Hendrity

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//echo "Hello World";
+$nama = " Bagas :v ";
+$umur = " 14 ";
+$IPK = " 4.00 ";
+echo "halo, namaku" . $nama . "umur kawanku" . $umur . "IPK dosen ku" . $IPK;
+
+echo "<br />";
+
+if ($IPK != 3.96){
+	echo " Hendrity sejati ";
+}
+
+
+
+
+
+?>
